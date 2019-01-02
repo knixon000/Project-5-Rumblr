6550477bf582430c48c43126cc86ba827b5e4750
Kendra Nixon <knixon000@nyc.yearup.org>
2019-01-02 09:17:36 -0500

    Deploying to Heroku updates

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require "sinatra/reloader"
 
 # Run this script with `bundle exec ruby app.rb`
-require 'sqlite3'
+
 require 'active_record'
 
 #require model classes
@@ -15,10 +15,17 @@ require 'csv'
 
 # Connect to a sqlite3 database
 # If you feel like you need to reset it, simply delete the file sqlite makes
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  database: 'db/development.db'
-)
+if ENV['DATABASE_URL']
+  require 'pg'
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  #Use sqlite since this is my computer
+  require 'sqlite3'
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: 'db/development.db'
+  )
+end
 
 register Sinatra::Reloader
 enable :sessions
